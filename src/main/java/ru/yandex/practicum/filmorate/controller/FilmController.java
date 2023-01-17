16800f7ac5386f8ac1e65c769e01d70bbbd1829f
uskov <spankynton1@yandex.ru>
2023-01-18 01:12:42 +0300

    Добавление эндпоинтов для UserController и FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,34 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    Map<Integer, Film> filmsCollection = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> getFilms() {
+        return this.filmsCollection.values();
+    }
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        filmsCollection.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilmInfo(@RequestBody Film film) {
+        filmsCollection.put(film.getId(), film);
+        return film;
+    }
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,7 +1,32 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    Map<Integer, User> users = new HashMap<>();
+    @GetMapping
+    public Collection<User> getUsers() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUserInfo(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
+
 }
