0c483ef695bace7e4710ffb75f4f11a49d87fee8
YaroslavAlekseenko <yar.a.alekseenko@yandex.ru>
2023-03-23 22:27:46 +0300

    "fix: вынести "/reviews" в аннотацию @RequestMapping в классе ReviewController." "fix: убрать "value =" в классе ReviewController."

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -12,59 +12,60 @@ import java.util.Map;
 
 @Slf4j
 @RestController
+@RequestMapping("/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
 
-    @GetMapping(value = "/reviews")
+    @GetMapping
     public List<Review> getReviewsByParams(@RequestParam(required = false) Map<String, Integer> params) {
         log.info("Endpoint request received: 'GET .reviews with params: {}'", params.toString());
         return reviewService.findReviewsByParams(params);
     }
 
-    @PostMapping(value = "/reviews")
+    @PostMapping
     public Review create(@Valid @RequestBody Review review) {
         log.info("Endpoint request received: 'POST reviews'");
         return reviewService.create(review);
     }
 
-    @GetMapping(value = "/reviews/{reviewId}")
+    @GetMapping("/{reviewId}")
     public Review getReviewById(@PathVariable Long reviewId) {
         log.info("Endpoint request received: 'GET reviews/{}'", reviewId);
         return reviewService.getByReviewId(reviewId);
     }
 
-    @PutMapping(value = "/reviews")
+    @PutMapping
     public Review update(@Valid @RequestBody Review review) {
         log.info("Endpoint request received: 'PUT reviews/{}'", review.toString());
         return reviewService.update(review);
     }
 
-    @PutMapping(value = "reviews/{reviewId}/like/{userId}")
+    @PutMapping("/{reviewId}/like/{userId}")
     public void addLikeToReview(@PathVariable Integer reviewId, @PathVariable Integer userId) {
         log.info("Endpoint request received: 'PUT reviews/{}/like/{}'", reviewId, userId);
         reviewService.addLikeToReview(reviewId, userId);
     }
 
-    @PutMapping(value = "reviews/{reviewId}/dislike/{userId}")
+    @PutMapping("/{reviewId}/dislike/{userId}")
     public void addDisLikeToReview(@PathVariable Integer reviewId, @PathVariable Integer userId) {
         log.info("Endpoint request received: 'PUT reviews/{}/dislike/{}'", reviewId, userId);
         reviewService.addDisLikeToReview(reviewId, userId);
     }
 
-    @DeleteMapping(value = "reviews/{reviewId}/like/{userId}")
+    @DeleteMapping("/{reviewId}/like/{userId}")
     public void deleteLikeToReview(@PathVariable Integer reviewId, @PathVariable Integer userId) {
         log.info("Endpoint request received: 'DELETE reviews/{}/like/{}'", reviewId, userId);
         reviewService.deleteLikeToReview(reviewId, userId);
     }
 
-    @DeleteMapping(value = "reviews/{reviewId}/dislike/{userId}")
+    @DeleteMapping("/{reviewId}/dislike/{userId}")
     public void deleteDisLikeToReview(@PathVariable Integer reviewId, @PathVariable Integer userId) {
         log.info("Endpoint request received: 'DELETE reviews/{}/dislike/{}'", reviewId, userId);
         reviewService.deleteDisLikeToReview(reviewId, userId);
     }
 
-    @DeleteMapping(value = "reviews/{reviewId}")
+    @DeleteMapping("/{reviewId}")
     public void removeReview(@PathVariable Long reviewId) {
         log.info("Endpoint request received: 'DELETE reviews/{}'", reviewId);
         reviewService.delete(reviewId);
